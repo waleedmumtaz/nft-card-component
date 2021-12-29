26afc216906c95f2d9745a718fb925ae7824db0b
Waleed <waleedthedev@gmail.com>
2021-12-29 19:09:43 +0500

    Updated page title

--- src/App.svelte
+++ src/App.svelte
@@ -3,6 +3,7 @@
 </script>
 
 <svelte:head>
+  <title>NFT Card Component</title>
   <link rel="preconnect" href="https://fonts.googleapis.com" />
   <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin="true" />
   <link

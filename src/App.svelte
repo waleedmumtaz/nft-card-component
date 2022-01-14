8aad398e0466b242da8a25262b92dcf574bfd671
Waleed <waleedthedev@gmail.com>
2022-01-14 16:48:52 +0500

    Testing meta tags

--- src/App.svelte
+++ src/App.svelte
@@ -1,5 +1,5 @@
 <script>
-  import { MetaTags } from 'svelte-meta-tags'
+  // import { MetaTags } from 'svelte-meta-tags'
   import NFTCard from './components/NFTCard.svelte'
 </script>
 
@@ -10,9 +10,49 @@
     href="https://fonts.googleapis.com/css2?family=Outfit:wght@300;400;600&display=swap"
     rel="stylesheet"
   />
+
+  <!-- Primary Meta Tags -->
+  <title>NFT Card Component</title>
+  <meta name="title" content="NFT Card Component" />
+  <meta
+    name="description"
+    content="NFT Card Component built with Svelte and Tailwind CSS"
+  />
+
+  <!-- Open Graph / Facebook -->
+  <meta property="og:type" content="website" />
+  <meta
+    property="og:url"
+    content="https://nft-card-component-seven.vercel.app/"
+  />
+  <meta property="og:title" content="NFT Card Component" />
+  <meta
+    property="og:description"
+    content="NFT Card Component built with Svelte and Tailwind CSS"
+  />
+  <meta
+    property="og:image"
+    content="https://res.cloudinary.com/dev-waleed/image/upload/v1642157085/Frontend%20Mentor/Meta%20Images/001_nft-card-component_1200x628.png"
+  />
+
+  <!-- Twitter -->
+  <meta property="twitter:card" content="summary_large_image" />
+  <meta
+    property="twitter:url"
+    content="https://nft-card-component-seven.vercel.app/"
+  />
+  <meta property="twitter:title" content="NFT Card Component" />
+  <meta
+    property="twitter:description"
+    content="NFT Card Component built with Svelte and Tailwind CSS"
+  />
+  <meta
+    property="twitter:image"
+    content="https://res.cloudinary.com/dev-waleed/image/upload/v1642157085/Frontend%20Mentor/Meta%20Images/001_nft-card-component_1200x628.png"
+  />
 </svelte:head>
 
-<MetaTags
+<!-- <MetaTags
   title="NFT Card Component"
   description="NFT Card Component built with Svelte and Tailwind CSS"
   openGraph={{
@@ -26,7 +66,7 @@
       },
     ],
   }}
-/>
+/> -->
 
 <main class="w-screen min-h-screen grid bg-cstm-neutral-dark-blue-main-bg">
   <NFTCard />

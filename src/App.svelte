150edb44f6508d138f5cf8614eb0907e641ef338
Waleed <waleedthedev@gmail.com>
2022-01-14 17:08:33 +0500

    Meta tags now work after adding them to index.html file, removed unused code and packages

--- src/App.svelte
+++ src/App.svelte
@@ -1,5 +1,4 @@
 <script>
-  // import { MetaTags } from 'svelte-meta-tags'
   import NFTCard from './components/NFTCard.svelte'
 </script>
 
@@ -11,22 +10,6 @@
     rel="stylesheet"
   />
 </svelte:head>
-
-<!-- <MetaTags
-  title="NFT Card Component"
-  description="NFT Card Component built with Svelte and Tailwind CSS"
-  openGraph={{
-    url: 'https://nft-card-component-seven.vercel.app/',
-    title: 'NFT Card Component',
-    description: 'NFT Card Component built with Svelte and Tailwind CSS',
-    images: [
-      {
-        url: 'https://res.cloudinary.com/dev-waleed/image/upload/v1642157085/Frontend%20Mentor/Meta%20Images/001_nft-card-component_1200x628.png',
-        alt: 'NFT Card Component',
-      },
-    ],
-  }}
-/> -->
 
 <main class="w-screen min-h-screen grid bg-cstm-neutral-dark-blue-main-bg">
   <NFTCard />
